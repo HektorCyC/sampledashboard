2836fdb0786325feea58c22fccb83d3e9eb13963
Héctor C <hcamarillo@ids.com.mx>
2019-09-12 13:03:52 -0500

    fix(post-list): fixed

--- src/app/components/postslist/postslist.component.ts
+++ src/app/components/postslist/postslist.component.ts
@@ -1,12 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NetworkService } from 'src/app/core/network.service';
-import { Observable } from 'rxjs';
 import { MatTableDataSource } from '@angular/material';
 
 export interface PostBody {
-  user: string;
-  title: string;
-  postBody: string;
+    documents: Array<string>;
 }
 
 
@@ -18,13 +15,13 @@ export interface PostBody {
 
 export class PostslistComponent implements OnInit {
   dataSource = new MatTableDataSource();
-  private entries: PostBody[] = [];
   constructor(private network: NetworkService) {
   }
 
   ngOnInit() {
-    this.network.getRequest('https://tratodemo.herokuapp.com/api').subscribe((res: PostBody[]) => {
+    this.network.getRequest('https://tratodemo.herokuapp.com/api').subscribe((res: PostBody) => {
       this.dataSource.data = res.documents;
+      console.log(this.dataSource.data)
     });
   }
   displayedColumns: string[] = ["nombre", "titulo"];
